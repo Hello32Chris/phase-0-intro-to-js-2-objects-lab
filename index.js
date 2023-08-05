2b46a08934ec9316c828467e10c4261e8d3a79b0
Christopher Orth <christopherorth430@yahoo.com>
2023-08-05 19:56:40 -0400

    Object labs test done!

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Write your solution in this file!
+const employee = {
+    name:"",
+    streetAddress:"",
+};
+
+function updateEmployeeWithKeyAndValue (obj, key, value) {
+    let newObj = {...obj}
+    newObj[key] = value;
+    console.log(newObj);
+    return newObj;
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    console.log(obj);
+    return obj;
+};
+
+function deleteFromEmployeeByKey(obj, key) {
+    let newObj = {...obj}
+    delete newObj[key];
+    return newObj;
+};
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    obj[key] = key;
+    delete obj[key];
+    return obj;
+};
